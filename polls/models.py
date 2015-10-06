cfc1afcbfd039cd514bfd6cacc2ce854b246b191
Rounaq <rounaqwl66@gmail.com>
2015-10-06 09:18:10 +0530

    Admin Control

--- polls/models.py
+++ polls/models.py
@@ -4,14 +4,16 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
+	question_text = models.CharField('question',max_length=200)
 	pub_date = models.DateTimeField('date published')
-
 	def __unicode__(self):
 		return self.question_text
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published Recently?'
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
